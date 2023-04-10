efeb2826c4753fe14acfc53b022a34c22d242381
eong0 <118071042+eong0@users.noreply.github.com>
2023-04-10 17:56:44 +0900

    Add files via upload

---
+++ cote3_2.py
@@ -0,0 +1,26 @@
+''' 큰 수의 법칙 '''
+
+#n, m, k 를 공백으로 구분해서 입력 받기
+n, m, k = map(int, input().split())
+
+#n개의 수를 공백으로 구분하여 입력받기
+data = list(map(int, input().split()))
+
+data.sort() #입력받은 수들을 정렬하기
+first = data[n-1] #가장 큰 수
+second = data[n-2] #두번째로 큰 수
+
+result = 0
+
+while True:
+    for i in range(k): #가장 큰 수를 k 번 더하기
+        if m == 0: #m이 0이라면 반복문 탈출
+            break
+        result += first
+        m -= 1 #더할 때마다 1씩 빼기
+    if m == 0: #m 이 0이라면 반복문 탈출
+        break
+    result += second #두번째로 큰 수를 한번 더하기
+    m -= 1 #더할 때마다 1씩 빼기
+
+print(result)
